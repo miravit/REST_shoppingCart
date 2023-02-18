6cd1126b1e48058c1fc4e12749a46a4c00355b76
Mira <miravitsas1@gmail.com>
2023-02-18 15:07:22 +0100

    quantity and totalamount fixed

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const Cart = require("../models/Cart");
 const { NotFoundError } = require("../utils/errors");
+const { CanceledError } = require("axios");
 
 exports.getAllProducts = async (req, res, next) => {
   const products = await Product.find();
@@ -25,26 +26,60 @@ exports.addProductToCart = async (req, res) => {
   const products = await Product.findById(productId);
   const cart = await Cart.findById(cartId);
 
-  if (!products) {
-    throw new NotFoundError("This product does not exist");
-  }
+  // if (!products) {
+  //   throw new NotFoundError("This product does not exist");
+  // }
 
-  if (!cart) {
-    throw new NotFoundError("This cart does not exist");
-  }
+  // if (!cart) {
+  //   throw new NotFoundError("This cart does not exist");
+  // }
   //console.log(cart);
 
   for (let i = 0; i < cart.products.length; i++) {
     if (cart.products[i]._id == productId) {
       cart.products[i].quantity++;
+      cart.totalAmount += cart.products[i].price;
       await cart.save();
       return res.status(201).json(cart);
     }
   }
+  cart.totalAmount += products.price;
+  console.log("hej");
   cart.products.push(products);
   await cart.save();
   return res.status(200).json(cart);
 };
 exports.deleteProductFromCart = async (req, res, next) => {
-  res.send("hola ta bort");
+  const productId = req.params.productId;
+  const cartId = req.body.cartId;
+
+  const product = await Product.findById(productId);
+  const cart = await Cart.findById(cartId);
+
+  // if (!product) {
+  //   throw new NotFoundError("This product does not exist");
+  // }
+
+  // if (!cart) {
+  //   throw new NotFoundError("This cart does not exist");
+  // }
+  //console.log(cart);
+
+  // for (let i = 0; i < cart.product.length; i++) {
+  //   if (cart.products[i].quantity == 1 && cart.product[i]._id == productId) {
+  //     console.log("tjena");
+  //     // cart.products[i].quantity--;
+  //     // await cart.save();
+  //     return res.status(201).json(cart);
+  //   }
+  for (let i = 0; i < cart.products.length; i++) {
+    if (cart.products[i]._id == productId) {
+      console.log("tjena");
+      // cart.products[i].quantity--;
+      // await cart.save();
+      // return res.status(200).json(cart);
+    }
+  }
+  await cart.save();
+  return res.status(200).json(cart);
 };
